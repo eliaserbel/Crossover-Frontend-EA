a134c8f1aa71f9b5c1e678ea92e78f3c2b32dbd2
estercalveras <estercalveras@gmail.com>
2023-10-28 17:21:44 +0200

    aa

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,3 +1,4 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import PropTypes from "prop-types"
 import 'leaflet/dist/leaflet.css';
 
@@ -15,12 +16,31 @@ const RestaurantList = ({ restaurants, onRestaurantSelect, selectedTag, selected
 
   return (
     <div className="restaurant-list">
+      <div className="restaurant-map">
       {restaurants.map((restaurant) => <div key={restaurant.id}>{restaurant.name}</div>)}
+
+        <MapContainer center={[40.7128, -74.0060]} zoom={13} style={{ height: '100vh', width: '100vw' }}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />          {filteredRestaurants.map(restaurant => (
+            <Marker
+              key={restaurant.id}
+              position={[restaurant.latitude, restaurant.longitude]}
+              eventHandlers={{
+                click: () => onRestaurantSelect(restaurant),
+              }}
+            >
+              <Popup>{restaurant.name}</Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+      </div>
 
       <div className="restaurant-items">
         {filteredRestaurants.map(restaurant => (
           <div key={restaurant.id}>{restaurant.name} {restaurant.tags.map((tag, index) => (<div key={index}>{tag}</div>))}</div>
-        ))}
+          ))}
       </div>
     </div>
   );
